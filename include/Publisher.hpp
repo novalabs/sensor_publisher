572a8b2e8f47657bf9e44a50bd4cb51664d5a77a
Davide Rizzi <d.rizzi@novalabs.io>
2016-07-27 11:08:27 +0200

    Updated with provider, configuration and core::os

--- include/Publisher.hpp
+++ include/Publisher.hpp
@@ -4,166 +4,111 @@
  * subject to the License Agreement located in the file LICENSE.
  */
 
-#include <Core/MW/Publisher.hpp>
-#include <Core/MW/CoreNode.hpp>
-#include <Core/MW/CoreSensor.hpp>
+#pragma once
 
-#include <sensor_publisher/Configuration.hpp>
+#include <core/mw/Publisher.hpp>
+#include <core/mw/CoreNode.hpp>
+#include <core/mw/CoreSensor.hpp>
 
-#include <Configuration.hpp>
-/*
- * template <class A, class B>
-   struct TypeAdaptor {};
+#include <core/sensor_publisher/Configuration.hpp>
 
-   template <>
-   struct TypeAdaptor<GyroData_i16, common_msgs::XYZ_i16> {
-   static inline void
-   _(
-      const GyroData_i16&   from,
-      common_msgs::XYZ_i16& to
-   )
-   {
-    //		to.t = from.t;
-    to.x = from.x;
-    to.y = from.y;
-    to.z = from.z;
-   }
+#include <ModuleConfiguration.hpp>
 
-   static inline void
-   _(
-      const GyroData_i16&   from,
-      common_msgs::XYZ_i16* to
-   )
-   {
-    //to->t = from.t;
-    to->x = from.x;
-    to->y = from.y;
-    to->z = from.z;
-   }
-
-   static inline void
-   _(
-      const common_msgs::XYZ_i16& from,
-      GyroData_i16&               to
-   )
-   {
-    //		to.t = from.t;
-    to.x = from.x;
-    to.y = from.y;
-    to.z = from.z;
-   }
-
-   static inline void
-   _(
-      const common_msgs::XYZ_i16& from,
-      GyroData_i16*               to
-   )
-   {
-    //to->t = from.t;
-    to->x = from.x;
-    to->y = from.y;
-    to->z = from.z;
-   }
-   };
- * */
-
+namespace core {
 namespace sensor_publisher {
-   template <class _DATATYPE, class _MESSAGETYPE>
-   struct Passthru {
-      static inline void
-      _(
-         const _DATATYPE& from,
-         _MESSAGETYPE*    to
-      )
-      {
-         * to = from;
-      }
-   };
-
-   template <class _DATATYPE, class _MESSAGETYPE = _DATATYPE, class _CONVERTER = Passthru<_DATATYPE, _MESSAGETYPE> >
-   class Publisher:
-      public Core::MW::CoreNode
+template <class _DATATYPE, class _MESSAGETYPE>
+struct Passthru {
+   static inline void
+   _(
+      const _DATATYPE& from,
+      _MESSAGETYPE*    to
+   )
    {
+      * to = from;
+   }
+};
+
+template <class _DATATYPE, class _MESSAGETYPE = _DATATYPE, class _CONVERTER = Passthru<_DATATYPE, _MESSAGETYPE> >
+class Publisher:
+   public core::mw::CoreNode,
+   public core::mw::CoreConfigurable<core::sensor_publisher::Configuration>
+{
 public:
-      using DataType    = _DATATYPE;
-      using MessageType = _MESSAGETYPE;
-      using Converter   = _CONVERTER;
+   using DataType    = _DATATYPE;
+   using MessageType = _MESSAGETYPE;
+   using Converter   = _CONVERTER;
 
 public:
-      Publisher(
-         const char*                     name,
-         Core::MW::CoreSensor<DataType>& sensor,
-         Core::MW::Thread::Priority      priority = Core::MW::Thread::PriorityEnum::NORMAL
-      ) :
-         CoreNode::CoreNode(name, priority),
-         _sensor(sensor)
-      {
-         _workingAreaSize = 256;
-      }
+   Publisher(
+      const char*                     name,
+      core::mw::CoreSensor<DataType>& sensor,
+      core::os::Thread::Priority      priority = core::os::Thread::PriorityEnum::NORMAL
+   ) :
+      CoreNode::CoreNode(name, priority),
+      CoreConfigurable<core::sensor_publisher::Configuration>::CoreConfigurable(name),
+      _sensor(sensor)
+   {
+      _workingAreaSize = 256;
+   }
 
-      virtual
-      ~Publisher()
-      {
-         teardown();
-      }
-
-public:
-      Configuration configuration;
+   virtual
+   ~Publisher()
+   {
+      teardown();
+   }
 
 private:
-      Core::MW::Publisher<MessageType> _publisher;
-      Core::MW::CoreSensor<DataType>&  _sensor;
+   core::mw::Publisher<MessageType> _publisher;
+   core::mw::CoreSensor<DataType>&  _sensor;
 
 private:
-      bool
-      onPrepareMW()
-      {
-         this->advertise(_publisher, configuration.topic);
+   bool
+   onPrepareMW()
+   {
+      this->advertise(_publisher, configuration().topic);
 
-         return true;
+      return true;
+   }
+
+   bool
+   onPrepareHW()
+   {
+      return _sensor.init();
+   }
+
+   bool
+   onStart()
+   {
+      return _sensor.start();
+   }
+
+   bool
+   onLoop()
+   {
+      MessageType* msgp;
+      DataType     tmp;
+
+      if (_sensor.waitUntilReady()) {
+         _sensor.update();
+         _sensor.get(tmp);
       }
 
-      bool
-      onPrepareHW()
-      {
-         return _sensor.init();
-      }
+      if (_publisher.alloc(msgp)) {
+         Converter::_(tmp, msgp);
 
-      bool
-      onStart()
-      {
-         bool ret = _sensor.start();
-
-
-         return ret;
-      }
-
-      bool
-      onLoop()
-      {
-         MessageType* msgp;
-         DataType     tmp;
-
-         if (_sensor.waitUntilReady()) {
-            _sensor.update();
-            _sensor.get(tmp);
+         if (!_publisher.publish(*msgp)) {
+            return false;
          }
-
-         if (_publisher.alloc(msgp)) {
-            Converter::_(tmp, msgp);
-
-            if (!_publisher.publish(*msgp)) {
-               return false;
-            }
-         }
-
-         return true;
-      } // onLoop
-
-      bool
-      onStop()
-      {
-         return _sensor.stop();
       }
-   };
+
+      return true;
+   }       // onLoop
+
+   bool
+   onStop()
+   {
+      return _sensor.stop();
+   }
+};
+}
 }
