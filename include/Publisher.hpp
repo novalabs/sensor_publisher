19ea14ac57b15f270e821791dee5758d40c9ffbe
Davide Rizzi <d.rizzi@novalabs.io>
2017-05-16 14:06:09 +0200

    ...

--- include/Publisher.hpp
+++ include/Publisher.hpp
@@ -18,104 +18,103 @@ namespace core {
 namespace sensor_publisher {
 template <class _DATATYPE, class _MESSAGETYPE>
 struct Passthru {
-   static inline void
-   _(
-      const _DATATYPE& from,
-      _MESSAGETYPE*    to
-   )
-   {
-      * to = from;
-   }
+    static inline void
+    _(
+        const _DATATYPE& from,
+        _MESSAGETYPE*    to
+    )
+    {
+        * to = from;
+    }
 };
 
 template <class _DATATYPE, class _MESSAGETYPE = _DATATYPE, class _CONVERTER = Passthru<_DATATYPE, _MESSAGETYPE> >
 class Publisher:
-   public core::mw::CoreNode,
-   public core::mw::CoreConfigurable<core::sensor_publisher::Configuration>
+    public core::mw::CoreNode,
+    public core::mw::CoreConfigurable<core::sensor_publisher::Configuration>
 {
 public:
-   using DataType    = _DATATYPE;
-   using MessageType = _MESSAGETYPE;
-   using Converter   = _CONVERTER;
+    using DataType    = _DATATYPE;
+    using MessageType = _MESSAGETYPE;
+    using Converter   = _CONVERTER;
 
 public:
-   Publisher(
-      const char*                         name,
-      core::utils::BasicSensor<DataType>& sensor,
-      core::os::Thread::Priority          priority = core::os::Thread::PriorityEnum::NORMAL
-   ) :
-      CoreNode::CoreNode(name, priority),
-      CoreConfigurable<core::sensor_publisher::Configuration>::CoreConfigurable(name),
-      _sensor(sensor)
-   {
-      _workingAreaSize = 256;
-   }
+    Publisher(
+        const char*                         name,
+        core::utils::BasicSensor<DataType>& sensor,
+        core::os::Thread::Priority          priority = core::os::Thread::PriorityEnum::NORMAL
+    ) :
+        CoreNode::CoreNode(name, priority),
+        CoreConfigurable<core::sensor_publisher::Configuration>::CoreConfigurable(name),
+        _sensor(sensor)
+    {
+        _workingAreaSize = 256;
+    }
 
-   virtual
-   ~Publisher()
-   {
-      teardown();
-   }
-
-private:
-   core::mw::Publisher<MessageType>    _publisher;
-   core::utils::BasicSensor<DataType>& _sensor;
+    virtual
+    ~Publisher()
+    {
+        teardown();
+    }
 
 private:
-   bool
-   onConfigure()
-   {
-      return isConfigured();
-   }
+    core::mw::Publisher<MessageType>    _publisher;
+    core::utils::BasicSensor<DataType>& _sensor;
 
+private:
+    bool
+    onConfigure()
+    {
+        return isConfigured();
+    }
 
-   bool
-   onPrepareMW()
-   {
-      this->advertise(_publisher, configuration().topic);
+    bool
+    onPrepareMW()
+    {
+        this->advertise(_publisher, configuration().topic);
 
-      return true;
-   }
+        return true;
+    }
 
-   bool
-   onPrepareHW()
-   {
-      return _sensor.init();
-   }
+    bool
+    onPrepareHW()
+    {
+        return _sensor.init();
+    }
 
-   bool
-   onStart()
-   {
-      return _sensor.start();
-   }
+    bool
+    onStart()
+    {
+        return _sensor.start();
+    }
 
-   bool
-   onLoop()
-   {
-      MessageType* msgp;
-      DataType     tmp;
+    bool
+    onLoop()
+    {
+        MessageType* msgp;
+        DataType     tmp;
 
-      if (_sensor.waitUntilReady()) {
-         _sensor.update();
-         _sensor.get(tmp);
-      }
+        if (_sensor.waitUntilReady()) {
+            _sensor.update();
+            _sensor.get(tmp);
+        }
 
-      if (_publisher.alloc(msgp)) {
-         Converter::_(tmp, msgp);
+        if (_publisher.alloc(msgp)) {
+            Converter::_(tmp, msgp);
 
-         if (!_publisher.publish(*msgp)) {
-            return false;
-         }
-      }
+            if (!_publisher.publish(*msgp)) {
+                return false;
+            }
+        }
 
-      return true;
-   }       // onLoop
+        return true;
+    }      // onLoop
 
-   bool
-   onStop()
-   {
-      return _sensor.stop();
-   }
+    bool
+    onStop()
+    {
+        return _sensor.stop();
+    }
 };
 }
 }
